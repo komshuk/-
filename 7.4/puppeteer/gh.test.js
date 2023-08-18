19d8a7392b2733e38c922bfe7a579c6c1e3bbf3e
komshuk <komshuk@yandex.ru>
2023-08-18 18:34:42 +0300

    Initian commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,34 +1,71 @@
 let page;
 
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://github.com/team");
-});
-
 afterEach(() => {
   page.close();
 });
 
-describe("Github page tests", () => {
+describe("Github/team page tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForTimeout(1000);
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
+  }, 10000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 6000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  }, 7000);
+});
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
   });
+
+  test("The sign in page test", async () => {
+    await page.goto("https://github.com");
+    const signInBtn = 'a[href="/login"]';
+    await page.waitForSelector(signInBtn);
+    await page.click(signInBtn);
+    await page.waitForTimeout(5000);
+    const actual = await page.title();
+    expect(actual).toContain("Sign in to GitHub · GitHub");
+  }, 10000);
+
+  test("Should open the pricing page", async () => {
+    await page.goto("https://github.com");
+    const pricingBtn = "nav > ul > li:nth-child(4) > a";
+    await page.waitForSelector(pricingBtn);
+    await page.click(pricingBtn);
+    await page.waitForTimeout(5000);
+    const articleTitleSelector = "h1";
+    await page.waitForSelector(articleTitleSelector);
+    const actual = await page.title();
+    expect(actual).toContain("Pricing · Plans for every developer · GitHub");
+  }, 7000);
+
+  test("Should open 'choose the enterprise plan' title", async () => {
+    await page.goto(
+      "https://github.com/organizations/enterprise_plan?ref_cta=Start+a+free+enterprise+trial&ref_loc=Home+campaign+hero&ref_page=%2F"
+    );
+    const choosePlan = await page.title();
+    expect(choosePlan).toContain("Choose an Enterprise plan · GitHub");
+  }, 7000);
 });
