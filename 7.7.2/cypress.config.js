d5d8138e74a6789379df5f24fa672b55522de273
komshuk <komshuk@yandex.ru>
2023-08-31 22:12:00 +0300

    first commit

--- 7.7.2/cypress.config.js
+++ 7.7.2/cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    projectId: "h6azhg",
     baseUrl: "https://petstore.swagger.io/v2/user",
     setupNodeEvents(on, config) {
       // implement node event listeners here
